c32216ebc99c50abc92edbfb6fa760017b3077f8
Victor Uma <umavictor11@gmail.com>
2023-07-01 08:59:03 +0100

    feat(config-files): configuration for tailwind and assets upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        black: 'rgba(0, 0, 0, 0.6)',
+        dark: 'black',
+        'primary-orange': '#FF5722',
+        blue: 'rgba(192, 225, 233, 1)'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
       },
     },
   },
