ddb760bf61d12f80b18aa4af34c5f63bbb1bf010
Victor Uma <umavictor11@gmail.com>
2023-07-28 13:40:17 +0100

    feat(Pagination-component): Added Pagination feature to stores page

--- app/stores/page.tsx
+++ app/stores/page.tsx
@@ -7,12 +7,24 @@ import { useState, useEffect } from 'react'
 import Storecard from '@components/stores/storecard'
 import AppLoader from '@components/global/AppLoader'
 import { userWallet } from '@app/StoreManager'
-const Storepage = () => {
+import { BASE_URL } from '@components/util/config'
+import PaginationControls from '@components/stores/PaginationControls'
+const Storepage = ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) => {
   const [allStores, setAllStores] = useState([])
   const [loading, setLoading] = useState(true)
   const { wallet } = userWallet.getState()
-  const baseUrl = 'https://store-hub-djxu.onrender.com/api/v1/'
-  // Get all stores
+
+  const page = searchParams['page'] ?? '1'
+  const page_size = searchParams['per_page'] ?? '15'
+
+  // Get all store
+  const handlePageLoading = () => {
+    setLoading(true)
+  }
 
   const getStoreOptions = {
     method: 'GET',
@@ -23,7 +35,10 @@ const Storepage = () => {
 
   const fetchAllStores = () => {
     try {
-      fetch(baseUrl + 'stores', getStoreOptions)
+      fetch(
+        `${BASE_URL}/stores/?page=${Number(page)}&page_size=${page_size}`,
+        getStoreOptions,
+      )
         .then((response) => response.json())
         .then((data) => {
           console.log(data, 'all stores')
@@ -38,53 +53,61 @@ const Storepage = () => {
 
   useEffect(() => {
     fetchAllStores()
-  }, [])
+  }, [page])
 
   return (
-    <div className="border border-black pt-8">
-      <div className="mx-8">
-        {wallet.accountId ? (
-          <>
-            <div></div>
-          </>
-        ) : (
-          <>
-            <div className="bg-blue p-6">
-              <p>
-                Powered by the NEAR blockchain, Store Hub offers secure,
-                user-friendly, and infinitely scalable solutions for seamless
-                operations. Revolutionize your digital presence with Store Hub.
-              </p>
-              <button type="button" className="outline_btn mt-6 font-medium">
-                <p className="hover:text-white items-baseline leading-tight text-black text-[16px]">
-                  Connect Wallet
+    <div>
+      <div className="border border-black pt-8">
+        <div className="mx-8">
+          {wallet.accountId ? (
+            <>
+              <div></div>
+            </>
+          ) : (
+            <>
+              <div className="bg-blue p-6">
+                <p>
+                  Powered by the NEAR blockchain, Store Hub offers secure,
+                  user-friendly, and infinitely scalable solutions for seamless
+                  operations. Revolutionize your digital presence with Store
+                  Hub.
                 </p>
-              </button>
-            </div>
-          </>
-        )}
+                <button type="button" className="outline_btn mt-6 font-medium">
+                  <p className="hover:text-white items-baseline leading-tight text-black text-[16px]">
+                    Connect Wallet
+                  </p>
+                </button>
+              </div>
+            </>
+          )}
 
-        <div>
           <div>
-            <div className="flex justify-between pt-8 mx-2">
-              <div>12 stores</div>
-              <div>
-                <Image src={sorticon} alt="sorticon" />
+            <div>
+              <div className="flex justify-between pt-8 mx-2">
+                <div>12 stores</div>
+                <div>
+                  <Image src={sorticon} alt="sorticon" />
+                </div>
               </div>
             </div>
+            {loading ? (
+              <AppLoader />
+            ) : (
+              <Stack gap={3}>
+                {allStores.map((store, index) => (
+                  <Storecard key={index} store={store} />
+                ))}
+                <div></div>
+              </Stack>
+            )}
           </div>
-          {loading ? (
-            <AppLoader />
-          ) : (
-            <Stack gap={3}>
-              {allStores.map((store, index) => (
-                <Storecard key={index} store={store} />
-              ))}
-              <div></div>
-            </Stack>
-          )}
         </div>
       </div>
+      <PaginationControls
+        hasNextPage={allStores.length > 1}
+        hasPrevPage={true}
+        handlePaginationLoading={handlePageLoading}
+      />
     </div>
   )
 }
