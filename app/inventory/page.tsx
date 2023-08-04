3606aaa9b98168cee999613fa468eba36f6e2348
dalu46 <lawrencefranklin100@gmail.com>
2023-08-04 11:54:04 +0100

    Enable user see the list of store they own in the inventory

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,114 +1,150 @@
-'use client'
-import { useRouter } from 'next/navigation'
-import { useSearchParams } from 'next/navigation'
-import { Key, useEffect, useState } from 'react'
-import { getSession } from '@components/util/session'
-import '../../styles/inventory.css'
-import StoreItem from '@components/stores/StoreItem'
-import { User } from '@app/StoreManager/userstore'
-import ProductItem from '@components/stores/productitem'
-import AddItemModal from '@components/stores/create-store/addItemModal'
-import AppLoader from '@components/global/AppLoader'
-import { BASE_URL } from '@components/util/config'
-import { ToastContainer, toast } from 'react-toastify'
-import { EditStoreProfile } from '@components/stores/create-store/EditStoreProfile'
+"use client";
+import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+import { Key, useEffect, useState } from "react";
+import { getSession } from "@components/util/session";
+import "../../styles/inventory.css";
+import StoreItem from "@components/stores/StoreItem";
+import { User } from "@app/StoreManager/userstore";
+import ProductItem from "@components/stores/productitem";
+import AddItemModal from "@components/stores/create-store/addItemModal";
+import AppLoader from "@components/global/AppLoader";
+import { BASE_URL } from "@components/util/config";
+import { ToastContainer, toast } from "react-toastify";
+import { EditStoreProfile } from "@components/stores/create-store/EditStoreProfile";
 
-import 'react-toastify/dist/ReactToastify.css'
-import { flattenDiagnosticMessageText } from 'typescript'
+import "react-toastify/dist/ReactToastify.css";
+import { flattenDiagnosticMessageText } from "typescript";
+
+interface Store {
+  category: string;
+  id: number;
+  name: string;
+  profile_image_url: string;
+  // Add other properties of a single store item as needed
+}
+
+type UserStores = {
+  data: {
+    message: string;
+    result: {
+      stores: Store[];
+    };
+  };
+  status: string;
+};
 const Inventory = () => {
-  const ID = '123PDWD'
-  const [storeItems, setStoreItems] = useState<any>([])
-  const [session, setSession] = useState<Session>()
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
-  const [isEditProfileModalOpen, setIsEditProfileModal] = useState<boolean>(false)
-  const [loading, setloading] = useState<boolean>(true)
-  const token = useSearchParams().get('token')
-  const userID = useSearchParams().get('user')
+  const ID = "123PDWD";
+  const [storeItems, setStoreItems] = useState<any>([]);
+  const [session, setSession] = useState<Session>();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isEditProfileModalOpen, setIsEditProfileModal] =
+    useState<boolean>(false);
+  const [userStores, setUserStores] = useState<Store[]>();
+  const [loading, setloading] = useState<boolean>(true);
+  const token = useSearchParams().get("token");
+  const userID = useSearchParams().get("user");
 
   const setAddItemStatus = (data: string) => {
-    if (data !== 'error') {
-      toast('Store item added successfully')
+    if (data !== "error") {
+      toast("Store item added successfully");
     } else {
-      toast.error('Error while adding item. Try again')
+      toast.error("Error while adding item. Try again");
     }
-  }
+  };
 
   const getStoreItemsOptions = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-  }
+  };
 
   const addStoreItemsOptions = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-  }
+  };
   // const searchParams = useSearchParams()
   const getStoreData = async () => {
     try {
       fetch(
         BASE_URL + `/users/${userID}/stores/${id}/items`,
-        getStoreItemsOptions,
+        getStoreItemsOptions
       )
         .then((response) => response.json())
         .then((data) => {
-          console.log(data, 'store items')
-          setStoreItems(data ? data.data.result.items : [])
-          setloading(false)
-        })
+          console.log(data, "store items");
+          setStoreItems(data ? data.data.result.items : []);
+          setloading(false);
+        });
     } catch (error) {
-      console.log(error, 'error from call')
+      console.log(error, "error from call");
     }
-  }
-  const id = useSearchParams().get('id')
-  const name = useSearchParams().get('name')
+  };
+  const id = useSearchParams().get("id");
+  const name = useSearchParams().get("name");
 
-  console.log(session, 'session')
+  console.log(session ? session.user : "hi", "session");
 
-  // Generate time options
-  const timeOptions: JSX.Element[] = []
-  for (let hour = 0; hour < 24; hour++) {
-    for (let minute = 0; minute < 60; minute += 15) {
-      const time = `${hour
-        .toString()
-        .padStart(2, '0')}:${minute.toString().padStart(2, '0')}`
-      timeOptions.push(
-        <option key={time} value={time}>
-          {time}
-        </option>,
-      )
-    }
-  }
+  //show a list of userStores in a dropdown
+  const getUserStores = async () => {
+    fetch(BASE_URL + `/users/${session ? session.user.user_id : ""}/stores`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${session ? session.access_token : ""}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data: UserStores) => {
+        console.log(data.data.result.stores);
+        setUserStores(data?.data?.result?.stores ?? []);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const userStoreList = userStores?.map((store: Store) => {
+    return (
+      <>
+        <option>{store.name}</option>
+      </>
+    );
+  });
+
   const readyItems = async () => {
-    let session = getSession()
-    setSession(session)
-  }
+    let session = getSession();
+    setSession(session);
+  };
 
   useEffect(() => {
+    getUserStores();
     readyItems().then(() => {
-      getStoreData()
-    })
-  }, [3])
+      getStoreData();
+    });
+  }, [3]);
 
   return (
     <main className="mb-6">
-      {
-        isEditProfileModalOpen ?
-      <div>
-        <EditStoreProfile hideModal={setIsEditProfileModal} session={session}/>
-      </div> : null
-}
+      {isEditProfileModalOpen ? (
+        <div>
+          <EditStoreProfile
+            hideModal={setIsEditProfileModal}
+            session={session}
+          />
+        </div>
+      ) : null}
       <div
         className={`modal ${
           isModalOpen
-            ? 'animate-animatefadeIn is-open'
-            : 'animate-animatefadeOut'
-        } lg:px-[] ${isModalOpen ? 'is-open' : ''}`}
+            ? "animate-animatefadeIn is-open"
+            : "animate-animatefadeOut"
+        } lg:px-[] ${isModalOpen ? "is-open" : ""}`}
       >
         <div className="modal-content w-[90%] md:w-[60%]">
           <div className="flex justify-between items-center lg:px-20px">
@@ -142,7 +178,11 @@ const Inventory = () => {
       </span>
       <div className="md:flex md:gap-5">
         <section className="flex gap-5 justify-between md:justify-start md:flex-col md:gap-5 mb-6 md:py-6">
-          <img src="../../assets/icons/Dashboard.svg" alt="Dashboard" onClick={() => setIsEditProfileModal(!isEditProfileModalOpen)}/>
+          <img
+            src="../../assets/icons/Dashboard.svg"
+            alt="Dashboard"
+            onClick={() => setIsEditProfileModal(!isEditProfileModalOpen)}
+          />
           <img src="../../assets/icons/user.svg" alt="user" />
           <img src="../../assets/icons/notification.svg" alt="notification" />
           <img src="../../assets/icons/money.svg" alt="money" />
@@ -155,10 +195,11 @@ const Inventory = () => {
             <p className="text-[18px] font-bold text-black mr-6">Products</p>
             <div className="flex gap-3 md:gap-5">
               <select className="block py-2 rounded-md border-0 px-1 md:px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm md:my-0">
-                <option>Option 1</option>
+                {/* <option>Option 1</option>
                 <option>Option 2</option>
                 <option>Option 3</option>
-                <option>Option 4</option>
+                <option>Option 4</option> */}
+                {userStoreList ? userStoreList : <option>Loading stores...</option>}
               </select>
               <select className="block py-2 rounded-md border-0 px-1 md:px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 md:my-0">
                 <option>Option 1</option>
@@ -168,7 +209,7 @@ const Inventory = () => {
               </select>
               <button
                 onClick={() => {
-                  setIsModalOpen(!isModalOpen)
+                  setIsModalOpen(!isModalOpen);
                 }}
                 className="block py-2 rounded-md border-0 px-1 md:px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 md:my-0"
               >
@@ -200,7 +241,7 @@ const Inventory = () => {
         </section>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Inventory
+export default Inventory;
