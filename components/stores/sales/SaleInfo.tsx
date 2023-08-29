f74c5de394e2b9ea6adee4973f81878ac3be7300
Victor Uma <umavictor11@gmail.com>
2023-08-29 12:10:10 +0100

    sale info modal

--- components/stores/sales/SaleInfo.tsx
+++ components/stores/sales/SaleInfo.tsx
@@ -13,31 +13,65 @@ const SaleInfo = () => {
   const saleInfoStatus = modalstore((state) => state.isSaleInfoOpen)
   const setStateInfoStatus = modalstore((state) => state.setSaleInfoStatus)
   const saleInfo = modalstore((state) => state.saleInfo)
+
   return (
     <div>
       <Dialog open={saleInfoStatus} onOpenChange={setStateInfoStatus}>
         <DialogContent>
-          {/* <div>
-            <div>product information</div>
-            <div>
-              <p>Product name: Manny</p>
-              <p>ProductId</p>
+          <div className="border border-dotted rounded-md">
+            <div className="bg-lightgray text-dark p-2">
+              Product information
+            </div>
+            <div className="text-xs p-2">
+              <p>
+                Product name:{' '}
+                <span className="text-dark">{saleInfo.item_name}</span>
+              </p>
+              <p>
+                ProductId: <span className="text-dark">{saleInfo.item_id}</span>
+              </p>
             </div>
           </div>
-          <div className="w-full grid grid-cols-3 gap-4">
-            <div className="border border-dotted-2">01</div>
-            <div className="border border-dotted-2">02</div>
-            <div className="border border-dotted-2">03</div>
-            <div className="col-span-2 border border-dotted-2">04</div>
-            <div className="border border-dotted-2">05</div>
-            <div className="border border-dotted-2">06</div>
-            <div className="col-span-2 border border-dotted-2">07</div>
-          </div> */}
-
-          <DialogHeader>
-            <DialogTitle>Are you sure absolutely sure?</DialogTitle>
-            <DialogDescription>Data here</DialogDescription>
-          </DialogHeader>
+          <div className="border border-dotted rounded-md">
+            <div className="bg-lightgray text-dark p-2">
+              Pricing information
+            </div>
+            <div className="text-xs p-2">
+              <p>
+                Product price:{' '}
+                <span className="text-dark">{saleInfo.item_price}</span>
+              </p>
+              <p className="border-b pb-2">
+                Shipping fee: <span className="text-dark">300</span>
+              </p>
+              <p>
+                Total: <span className="text-dark">{saleInfo.item_price}</span>
+              </p>
+            </div>
+          </div>
+          <div className="border border-dotted rounded-md">
+            <div className="bg-lightgray text-dark p-2">Order details</div>
+            <div className="text-xs p-2">
+              <p>
+                Order id: <span className="text-dark">{saleInfo.order_id}</span>
+              </p>
+              <p>
+                Order date:{' '}
+                <span className="text-dark">{saleInfo.order_date}</span>
+              </p>
+            </div>
+          </div>
+          <div className="border border-dotted rounded-md">
+            <div className="bg-lightgray text-dark p-2">
+              Delivery information
+            </div>
+            <div className="text-xs p-2">
+              <p>
+                Delivery date:{' '}
+                <span className="text-dark">{saleInfo.delivery_date}</span>
+              </p>
+            </div>
+          </div>
         </DialogContent>
       </Dialog>
     </div>
