e13cbef6038c6aebbe0b580c8f6c5ac0692b20b2
dalu46 <lawrencefranklin100@gmail.com>
2023-09-26 08:46:26 +0100

    Updated shoppers UI

--- app/(auth)/auth/signup/page.tsx
+++ app/(auth)/auth/signup/page.tsx
@@ -183,6 +183,7 @@ const SignUp = () => {
                   >
                     {/* Checkmark SVG */}
                     <svg
+                    className='transform -rotate-90'
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
                       fill="none"
